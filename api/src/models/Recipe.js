cacc2d6398ee24d92929d7c1bc885018331fb9e7
RaulSierradev <illosierra@outlook.com>
2023-05-24 08:26:37 -0500

    Creado la sección de sequelize

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,40 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "recipe",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      food_sumary: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      health_score: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      preparation: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
