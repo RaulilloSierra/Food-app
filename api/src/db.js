cc49767c1ccf18f193ec728bf70ca23590f6b661
RaulilloSierra <raulitosierradiaz@gmail.com>
2023-09-10 16:51:40 -0500

    update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 
 const sequelize = new Sequelize(
   // URL
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {  logging: false, native: false }
 );
 const basename = path.basename(__filename);
